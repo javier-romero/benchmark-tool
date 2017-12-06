eb9ed8974685497a0367ad2f9597c4a42a1d5582
Javier Romero <javier@cs.uni-potsdam.de>
2017-12-06 07:51:45 +0100

    added tar compression to do_benchmarks

--- experiments/do_benchmarks.sh
+++ experiments/do_benchmarks.sh
@@ -30,12 +30,13 @@ echo "beval..."
 echo "bconv..."
 cat $dir/results/$name/$name.beval | ./bconv -m time,ctime,csolve,ground0,groundN,models,timeout,restarts,conflicts,choices,domain,vars,cons,mem,error,memout > $dir/results/$name/$name.ods 2>> $dir/results/$name/$name.error
 
-echo "mv..."
-mv output/$project/* $dir/results/$name
+echo "tar..."
+tar -czf $name.tar.gz output/$project
+mv $name.tar.gz $dir/results/$name
+rm -rf output/$project
 
 echo "done"
 echo $dir/results/$name/$name.ods
-
 # send an email to report that the experiments are done
 # echo "done $1" | mail -s "[benchmark_finished] $1 " -A $dir/results/$name/$name.ods <email>
 
